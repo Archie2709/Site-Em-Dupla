2552e2e6985286df4eb47568b77516875ab9846c
Archie2709 <arthurneto.alcantara@gmail.com>
2025-06-10 19:00:49 +0000

    merge

--- avic/apps.py
+++ avic/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class AlgumacoisaConfig(AppConfig):
+class AvicAppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "algumacoisa"
+    name = "avic
+    "
